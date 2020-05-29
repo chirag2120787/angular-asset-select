cacf698a0a151d8b8f432e862377dd12cc8841b0
Chirag Bansal <chirag.bansal@mecuris.com>
2020-05-29 13:19:39 +0200

    generate asset select component

---
+++ src/app/asset-select/asset-select.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asset-select',
+  templateUrl: './asset-select.component.html',
+  styleUrls: ['./asset-select.component.css']
+})
+export class AssetSelectComponent {
+
+}
